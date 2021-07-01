b9e265593857515d67bb22913e274ef3249c474f
Pongpanoth Panya <pongpanoth213@gmail.com>
2021-07-02 00:45:02 +0700

    lab 3.6

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data(){
         return{
             product:'Boots',
-            description:"Boots or Shoes, ScooBy-Doo"
+            description:"Boots or Shoes, ScooBy-Doo",
+            image:'./assets/images/socks_green.jpg',
+            link:'https://www.camt.cmu.ac.th/index.php/th/'
         }
     }
 })
