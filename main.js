e46d2e86ba240b90a923ebf7fbe4e78403896203
Pongpanoth Panya <pongpanoth213@gmail.com>
2021-07-02 01:01:28 +0700

    lab 5.4

--- main.js
+++ main.js
@@ -7,7 +7,21 @@ const app = Vue.createApp({
             link:'https://www.camt.cmu.ac.th/index.php/th/',
             inStock: true,
             inventory: 10,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton','30% wool','20% polyester'],
+            variants: [
+                {
+                    id:2234, 
+                    color:'green'
+                },
+                {
+                    id:2235, 
+                    color:'blue'
+                }
+            ],
+            sizes:[
+                'S','M','L'
+            ]
         }
     }
 })
